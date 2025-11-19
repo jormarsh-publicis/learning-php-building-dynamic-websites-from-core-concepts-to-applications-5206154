6a3b50c4b8729b2a760325d0f0286c84ccff7a07
jormarsh-publicis <jordan.marshall@publicis.com>
2025-11-19 16:03:32 +0000

    complete challenge 1

--- index.php
+++ index.php
@@ -4,3 +4,27 @@ $city = "NYC";
 $movie = "Moana 2"; //Italics
 $friends = "Lou and Abby"; //Bold
 $candy = "Jelly Beans";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Challenge 1</title>
+  <meta name="author" value="Jordan Marshall">
+</head>
+
+<body>
+  <?php
+
+  echo "<h1>Hello $name</h1>";
+
+  echo "<p> You will be seeing <em>$movie</em> today in $city";
+
+  echo "<p> $friends are also invited";
+
+  echo "<p> They are bringing <strong>$candy</strong> </p>"
+  ?>
+</body>
+
+</html>
